6c7a1dc95dc8ac41a293e2fa91711ad0911a4ba8
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-08-04 11:01:51 -0300

    Server - EditorConfig, ESLint e Prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,11 @@ import express, { json } from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-    console.log('Página acessada 😀');
+  console.log('Página acessada 😀');
 
-    response.json({
-        message: "Welcome to the first page!"
-    })
-})
+  response.json({
+    message: 'Welcome to the first page!',
+  });
+});
 
 app.listen(3333);
