74dccccba7fbe9c5cfdb0a10b33c7a57e0be36c8
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-08-09 11:48:02 -0300

    Mobile - Componente TeacherItem

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Image } from 'react-native';
+import {
+  Container,
+  Profile,
+  Avatar,
+  ProfileInfo,
+  Name,
+  Subject,
+  Bio,
+  Footer,
+  Price,
+  PriceValue,
+  ButtonsContainer,
+  FavoriteButton,
+  ContactButton,
+  ContactButtonText,
+} from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+const TeacherItem: React.FC = () => (
+  <Container>
+    <Profile>
+      <Avatar source={{ uri: 'https://github.com/magno193.png' }} />
+      <ProfileInfo>
+        <Name>Alexandre Ferreira</Name>
+        <Subject>Química</Subject>
+      </ProfileInfo>
+    </Profile>
+
+    <Bio>
+      Wannabe musician and full stack developer
+    </Bio>
+
+    <Footer>
+      <Price>
+        Preço/hora
+        {'   '}
+        <PriceValue>R$: 20.00</PriceValue>
+      </Price>
+
+      <ButtonsContainer>
+        <FavoriteButton>
+          <Image source={heartOutlineIcon} />
+        </FavoriteButton>
+
+        <ContactButton>
+          <Image source={whatsappIcon} />
+          <ContactButtonText>Entrar em contato</ContactButtonText>
+        </ContactButton>
+      </ButtonsContainer>
+    </Footer>
+  </Container>
+);
+export default TeacherItem;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Container } from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 const TeacherList:React.FC = () => (
   <Container>
-    <PageHeader title="Proffys disponíveis" />
+    <PageHeader
+      title="Proffys disponíveis"
+    />
+
+    <TeacherItem />
   </Container>
 );
 
